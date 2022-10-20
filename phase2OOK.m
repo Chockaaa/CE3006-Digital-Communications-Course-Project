3aa17672ce53170cc583186648ab083b88285c76
Chockalingam Kasi <chocka1998@gmail.com>
2022-10-20 22:08:58 +0800

    Update phase2OOK.m

--- phase2OOK.m
+++ phase2OOK.m
@@ -1,26 +1,26 @@
 %% Phase 2: Modulation for communication
 clc; close all; clear workspace;
 
-%Generated baseband data
+% Generated baseband data
 N_bits = 1024;
 
-%Let the carrier frequency be 10 KHz
+% Let the carrier frequency be 10 KHz
 Fc = 10000;
 
-%carrier signal is 16 times oversampled
+% Carrier signal is 16 times oversampled
 Fs = Fc * 16;
 
-%consider the baseband data rate as 1 kbps
+% onsider the baseband data rate as 1 kbps
 baseband_dataRate = 1000;
 Ts = Fs / baseband_dataRate; % sampling period
 
-%Modulate the data samples with carrier signal (cos(2pft))
+% Modulate the data samples with carrier signal (cos(2pft))
 A = 1; % multiplying twice the carrier signal
 t = 0: 1/Fs : N_bits/baseband_dataRate;
 carrier_sig = A .* cos(2*pi*Fc*t);
 
-%gen LPF
-%Assume a 6th order filter with cut-off frequency 0.2 in the function
+% Gen LPF
+% Assume a 6th order filter with cut-off frequency 0.2 in the function
 [b_low, a_low] = butter(6, 0.2);
 
 signalLen = Fs* N_bits /baseband_dataRate + 1;
@@ -28,27 +28,33 @@ SNR_db_Values_Array = 0:5:50; %0:5:50;
 ER_OOK = zeros(length(SNR_db_Values_Array));
 
 for k = 1:length(SNR_db_Values_Array)
-    SNR = (10.^(SNR_db_Values_Array(k)/10));
+    SNR = (10.^(SNR_db_Values_Array(k)/10));   
 
     avg_OOK_error = 0;
 
-    %generate data
-    for j = 1 : 10      % each SNR avg the error over x times
-       
+    % Generate data
+    for j = 1 : 10     % Each SNR avg the error over 10 times
         Data = round(rand(1,N_bits));
         
-        %fill the data stream
+
+        % Fill the data stream
         DataStream = zeros(1, signalLen);
+        
         for i = 1: signalLen - 1
             DataStream(i) = Data(ceil(i*baseband_dataRate/Fs));
+            
         end
+        
         DataStream(signalLen) = DataStream(signalLen - 1);
 
-   % OOK
+        % OOK
         OOK_Signal = carrier_sig .* DataStream;
 
-        % generate noise
+        
+
+        % Generate noise
         OOK_SignalPower = (norm(OOK_Signal)^2)/signalLen;
+        
         OOK_NoisePower_variance = OOK_SignalPower ./ SNR;
         OOK_Noise = sqrt(OOK_NoisePower_variance/2) .*randn(1,signalLen);
 
@@ -56,31 +62,24 @@ for k = 1:length(SNR_db_Values_Array)
 
         OOK_Squared = OOK_Signal_Received.^2; %square law device (detection)
 
-        % filtering of the demodulated signal
+        % Filtering of the demodulated signal
         OOK_Filtered = filtfilt(b_low, a_low, OOK_Squared);
 
         % Use the decision threshold logic for decoding of received signals
         OOK_Sampled = sample(OOK_Filtered, Ts, N_bits);
         
-        OOK_Result = zeros(1, N_bits);
-        for x = 1:N_bits
-            if (OOK_Sampled(x) > A/2)
-                OOK_Result(x) = 1;
-            else
-                OOK_Result(x) = 0;
-            end
-        end
-
-   % Calculate the bit error rate performance
-        OOK_Error = 0;
-
+        OOK_Result = decision_logic(OOK_Sampled,N_bits,A/2);
+        
+        % Calculate the bit error rate performance
+        OOK_Error = 0;    
+    
         for i = 1: N_bits - 1
             if(OOK_Result(i) ~= Data(i))
                 OOK_Error = OOK_Error + 1;
             end
-        end
+        end     
         avg_OOK_error = OOK_Error + avg_OOK_error;
-
+    
     end
 
     if(SNR_db_Values_Array(k) == 5)
@@ -101,15 +100,28 @@ title('Error rate performance for OOK');
 ylabel('Pe');
 xlabel('Eb/No');
 
-% plot the signals at different stages (data waveform, modulated
-% signal, received signal, demodulated signal and decoded signal) 
+% Plot the signals at different stages (data waveform, modulated
+% Signal, received signal, demodulated signal and decoded signal) 
 % for a selected SNR value
 figure(2);
-subplot(511);title('Generated Data');plot(plot_signal);
-subplot(512);title('Modulated OOK');plot(plot_mod_OOK,'k');
-subplot(513);title('Received Signal OOK');plot(plot_receive_OOK, 'k')
-subplot(514);title('Demodulated OOK');plot(plot_demod_OOK, 'k');
-subplot(515);title('Decoded Data');plot(plot_decoded_OOK);
+subplot(511);plot(plot_signal);title('Generated Data');
+subplot(512);plot(plot_mod_OOK,'k');title('Modulated OOK');
+subplot(513);plot(plot_receive_OOK, 'k');title('Received Signal OOK');
+subplot(514);plot(plot_demod_OOK, 'k');title('Demodulated OOK');
+subplot(515);plot(plot_decoded_OOK);title('Decoded Data');
+
+
+
+function Result_Out = decision_logic(sampled,N_bits,threshold)
+    Result_Out = zeros(1, N_bits);
+    for x = 1:N_bits
+        if (sampled(x) > threshold)
+            Result_Out(x) = 1;
+        else
+            Result_Out(x) = 0;
+        end
+    end
+end
 
 
 function sampled = sample(x, samplingPeriod, numBit)
